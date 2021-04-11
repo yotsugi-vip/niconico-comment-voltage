aec848e0070d43d8271649bb6a035e7ddc27c48c
yotsugi <dev01.topaz@gmail.com>
2021-04-11 10:25:50 +0900

    fix full screen mode

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
 const color_comment = "#4990E2";
-const color_nucoru = "#D9A300";
+const color_nicoru = "#D9A300";
 const color_back = "#252525";
+
+const addin_container = "nico2-comment-voltage-container";
+const addin_inner = "nico2-comment-voltage-inner";
+const canvas_container = "container";
+let canvas_id = "comment-voltage-canvas";
+
+/** @type {Array<Number>} */
+let json_data;
+
+/** @type {Array<Number>} */
+let comment_data;
+
+/** @type {Array<Number>} */
+let nicoru_data;
+
+/**
+ * 拡張機能情報出力
+ */
+ const titleInfo = () => {
+    const manifest = chrome.runtime.getManifest();
+    console.log(`${manifest.name}: version: ${manifest.version}`);
+}
 
 /** Request用Json作成 */
 const makeJson = () => {
@@ -69,7 +91,7 @@ const fetchComment = async () => {
 }
 
 /**
- * コメントを配列に格納
+ * コメント数を配列に格納
  * @param {JSON} data_json 
  * @returns {Array<number>} Array\<number>[100]
  */
@@ -90,6 +112,11 @@ const getCommentData = (data_json) => {
     return arr;
 }
 
+/**
+ * ニコる数を配列に格納
+ * @param {JSON} data_json 
+ * @returns {Array<number>} Array\<number>[100]
+ */
 const getNicoruData = (data_json) => {
     const api_data = $('#js-initial-watch-data').attr('data-api-data');
     const api_json = JSON.parse(api_data);
@@ -109,62 +136,70 @@ const getNicoruData = (data_json) => {
     return arr;
 }
 
-const titleInfo = () => {
-    //URL取得
-    var host = location.hostname;
-    var path = location.pathname;
-    var now_location = host + path + "";
-    console.log("NicoNico Comment Voltage");
+/**
+ * コメント取得
+ */
+const getData = async () => {
+    data_json = await fetchComment();
+    comment_data = getCommentData(data_json);
+    nicoru_data = getNicoruData(data_json);
 }
 
-/** メイン */
-$(async function () {
-
-    let data_json;
-    let comments;
-    let nicoru;
-
-    titleInfo();
-
-    data_json = await fetchComment();
-    comments = getCommentData(data_json);
-    nicoru = getNicoruData(data_json);
-
-    $('.ControllerContainer').before('<div class="adin"></div>');
-    $('.adin').css({
+/**
+ * 拡張機能用DOM差し込み
+ */
+const addDom = () => {
+    $('.ControllerContainer').before(`<div class="${addin_container}"></div>`);
+    $(`.${addin_container}`).css({
         "position": "relative",
         "top": 0,
         "right": 0,
         "bottom": 0,
         "left": 0,
-        "height": "50px"
+        "height": "50px",
+        "width": "100%"
     });
 
-    $('.adin').append('<div class="adin-inner"></div>');
-    $('.adin-inner').css({
+    $(`.${addin_container}`).append(`<div class="${addin_inner}"></div>`);
+    $(`.${addin_inner}`).css({
         "border-radius": "8px",
         "background-color": color_back,
         "height": "100%",
         "margin-top": "5px",
         "margin-left": "5px",
-        "margin-right": "5px"
+        "margin-right": "5px",
+        "width": "100% - 10px"
     });
 
-    $(".adin-inner").append('<div class="canvas-container"></div>');
-    $(".canvas-container").css({
+    $(`.${addin_inner}`).append(`<div class="${canvas_container}"></div>`);
+    $(`.${canvas_container}`).css({
         "height": "100%",
         "margin-left": "5px",
-        "margin-right": "5px"
+        "margin-right": "5px",
+        "width": "100% - 10px"
     });
+}
 
-    let w = $('.canvas-container').width();
-    let h = $('.canvas-container').height();
-
-    const canvas_id = "comment-voltage";
-    const canvas_raw = `<canvas id=\"${canvas_id}\" width=\"${w}\" height=\"${h}\"></canvas>`;
-
-    $('.canvas-container').append(canvas_raw);
+/**
+ * コメントボルテージ描画
+ */
+const drawVoltage = () => {
+    const w = $(`.${canvas_container}`).width();
+    const h = $(`.${canvas_container}`).height();
+    const canvas_dom = `<canvas id=\"${canvas_id}\" width=\"${w}\" height=\"${h}\"></canvas>`;
     let canvas = document.getElementById(canvas_id);
+
+    if (canvas) {
+        canvas.remove();
+    }
+
+    $(`.${canvas_container}`).append(canvas_dom);
+    $(`#${canvas_id}`).css({
+        "width": "100%",
+        "height": `${h}px`
+    })
+
+    canvas = document.getElementById(canvas_id);
 
     if (canvas.getContext) {
         /**@type {CanvasRenderingContext2D} */
@@ -175,14 +210,12 @@ $(async function () {
 
         let x = 0;
         let y = 0;
-        const width = w / duration;
+        let width = w / duration;
         let height = 0;
 
         // コメントデータ描画
         ctx.fillStyle = color_comment;
-        comments.forEach((c, i) => {
-            //height = (c / h) * 100;
-            //height = height > 100 ? 100 : height;
+        comment_data.forEach((c, i) => {
             height = c;
             x = i * width;
             y = h - height;
@@ -191,8 +224,8 @@ $(async function () {
         });
 
         // ニコるデータ描画
-        ctx.fillStyle = color_nucoru;
-        nicoru.forEach((c, i) => {
+        ctx.fillStyle = color_nicoru;
+        nicoru_data.forEach((c, i) => {
             height = (c / h) * 100;
             height = height > 100 ? 100 : height;
             x = i * width;
@@ -201,6 +234,43 @@ $(async function () {
             ctx.fillRect(x, y, width / 2, height);
         });
     }
+}
 
+/**
+ * 全画面の切り替え監視
+ */
+const observer = () => {
+    const body = document.body;
+    const manifest = chrome.runtime.getManifest();
+    const observer = new MutationObserver((mutations) => {
+        mutations.forEach((mutation) => {
+            /** @type {string} className*/
+            const className = mutation.target.className;
+            if (className.includes("is-fullscreen")) {
+                console.log(`${manifest.name}: 画面サイズ： フルスクリーン`);
+                drawVoltage();
+            } else {
+                console.log(`${manifest.name}: 画面サイズ： 通常`);
+                drawVoltage();
+            }
+        });
+    });
 
+    /** @type {MutationObserverInit}   */
+    const config = { attributes: true };
+    observer.observe(body, config);
+}
+
+/** メイン */
+$(async function () {
+
+    titleInfo();
+
+    addDom();
+
+    await getData();
+
+    drawVoltage();
+
+    observer();
 });
